3dca64b6b096a583dbc2bc28974fa63135b7f19a
Marouane El Aich <elaich.marouane@gmail.com>
2018-12-21 19:11:05 +0000

    FT updated we want unique URLs for each new list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -41,8 +44,7 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates and now the page lists
         # "1: Buy a peacock toy for my girl" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy a peacock toy for my girl')
+        self.wait_for_row_in_list_table('1: Buy a peacock toy for my girl')
 
         # There is still a text box inviting her to type more to-do items
         # She enters "Buy crayons"
@@ -50,17 +52,68 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy crayons')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items in the to-do list
-        self.check_for_row_in_list_table('1: Buy a peacock toy for my girl')
-        self.check_for_row_in_list_table('2: Buy crayons')
+        self.wait_for_row_in_list_table('1: Buy a peacock toy for my girl')
+        self.wait_for_row_in_list_table('2: Buy crayons')
 
-        # Edith wonder if the website will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-        self.fail('finish the test!')
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy a peacock toy')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy a peacock toy')
 
-        # She visits that URL - her to-do list is there.
+        # She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
 
-        # Satisfied she goes back to sleep.
+        # Now a new user Hamid, comes along to the site,
+
+        ## We use a new browsers session to ensure 
+        ## that no information from Edith's browser is 
+        ## coming through cookies or something
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Hamid visits the homepage. There is no sign
+        # of Edith's list
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy a peacock toy', page_text)
+        self.assertNotIn('Buy crayons', page_text)
+
+        # Hamid starts a new list by entering a new item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Take selfie with phone')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Take selfie with phone')
+
+        # Hamid gets his own unique URL
+        hamid_list_url = self.browser.current_url
+        self.assertRegex(hamid_list_url, '/lists/.+')
+        self.assertNotEqual(hamid_list_url, edith_list_url)
+
+        # Again there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy a peacock toy', page_text)
+        self.assertNotIn('Buy crayons', page_text)
+
+        # Satisfied they both go to sleep
+
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
